76663666059c2b2bf08e3ef280e349df06a01c7c
Jangbu Sherpa <jangbusherpa@Jangbus-MacBook-Air.local>
2023-11-24 09:24:42 -0500

    Added the map function to calculate the winning rate of each teams

--- Program.fs
+++ Program.fs
@@ -15,11 +15,11 @@ let coaches = [
 let teamNames = ["Atlanta Hawks"; "Boston Celtics"; "Brooklyn Nets"; "Charlotte Hornets"; "Chicago Bulls"]
 
 let stats = [
-    { Wins = 8; Losses = 7 };
-    { Wins = 0; Losses = 12 };
-    { Wins = 0; Losses = 6 };
-    { Wins = 7; Losses = 5 };
-    { Wins = 7; Losses = 5 }
+    { Wins = 7; Losses = 7 };
+    { Wins = 12; Losses = 3 };
+    { Wins = 6; Losses = 8 };
+    { Wins = 5; Losses = 9 };
+    { Wins = 5; Losses = 11}
 ]
 
 let teams =
@@ -36,4 +36,15 @@ let successfulTeams =
 successfulTeams |> List.iter (fun team ->
     printfn "%s\t%d-%d\t%s"
         team.Coach.Name team.Stats.Wins team.Stats.Losses team.Name
+)
+
+let scsPercent =
+    List.map (fun team ->
+        let totalGames = float (team.Stats.Wins + team.Stats.Losses)
+        let scsPercent1 = (float team.Stats.Wins) / totalGames * 100.0
+        (team, scsPercent1)
+    ) teams
+
+scsPercent |> List.iter (fun (team, percentage) ->
+    printfn "%s's Win Rate: %.2f%%" team.Name percentage
 )
